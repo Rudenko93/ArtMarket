5066b58924cd00d50bc9864af994de2b1800aeaa
Deni <dv04111993@yandex.ru>
2023-02-20 22:50:04 +0100

    TrustedBy component

--- client/src/pages/Home/Home.jsx
+++ client/src/pages/Home/Home.jsx
@@ -1,10 +1,12 @@
 import { Featured } from "../../components/Featured"
+import { TrustedBy } from "../../components/TrustedBy"
 import "./Home.scss"
 
 export function Home() {
   return (
     <div className="">
       <Featured />
+      <TrustedBy />
     </div>
   )
 }
